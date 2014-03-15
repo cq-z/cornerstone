03e2a5e73b52fa0a17c7f67b5ec37b92f219e2bf
zcq <64899484@qq.com>
2014-03-15 16:11:42 +0800

    beta

--- nginxsh/logcron.sh
+++ nginxsh/logcron.sh
@@ -12,19 +12,19 @@ remove=30
 ls ${log_dir}> /tmp/filename
 for file in `cat /tmp/filename`
 do
-if [ -d $file ]
+if [ -d ${log_dir}/${file} ]
 then
   if [ -s ${log_dir}/$file/access.log ]
   then
-    /bin/mv  ${log_dir}/$file/access.log ${log_dir}/$file/access${date}.log
+    /bin/mv  ${log_dir}/${file}/access.log ${log_dir}/${file}/access${date}.log
   fi
-  if [ -s ${log_dir}/$file/error.log ]
+  if [ -s ${log_dir}/${file}/error.log ]
   then
-    /bin/mv  ${log_dir}/$file/error.log ${log_dir}/$file/error${date}.log
+    /bin/mv  ${log_dir}/$file/error.log ${log_dir}/${file}/error${date}.log
   fi
 fi
 done
-find /data/logs/ -name '*.log'  -mtime +${remove} | xargs rm -f
+find ${log_dir}/ -name '*.log'  -mtime +${remove} | xargs rm -f
 
 #Reopen Nginx logs file
-kill -USR1 `cat  /usr/local/nginx/var/nginx.pid`f
+kill -USR1 `cat  /usr/local/nginx/var/nginx.pid`
